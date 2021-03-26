d3027c28fe9053374cade6fd7acc25e6ac8267a2
mattyImry <matteo.imeri@gmail.com>
2021-03-26 17:32:27 +0000

    Added Navbar

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_task():
+@app.route("/recipes")
+def recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
