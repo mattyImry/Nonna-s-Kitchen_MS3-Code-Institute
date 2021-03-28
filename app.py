8d72405793abb70e0de7249059fcd1b7335e0734
mattyImry <matteo.imeri@gmail.com>
2021-03-28 15:40:45 +0000

    Added registration functionality

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
+# CONFIGURATION
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# HOME PAGE
 @app.route("/")
 @app.route("/index")
 def index():
@@ -25,8 +27,27 @@ def index():
     return render_template("index.html", recipes=recipes)
 
 
+# REGISTER PAGE
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # checking of users exist in database
+        existing_user = mongo.db.user.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Sorry Username already exist, try again")
+            return render_template("register.html")
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Thank you! You are now registered")
     return render_template("register.html")
 
 
